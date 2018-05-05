2b8acd782a088c8bb3f3ba053bbc594f10a44bfd
root676 <clemens.raffler@gmx.at>
2018-05-05 14:15:54 +0200

    add better help message in dummy alg

--- algs/DummyAlgorithm.py
+++ algs/DummyAlgorithm.py
@@ -35,7 +35,8 @@ pluginPath = os.path.split(os.path.split(os.path.dirname(__file__))[0])[0]
 
 class DummyAlgorithm(QgisAlgorithm):
 
-    MESSAGE = 'MESSAGE'
+    MESSAGE1 = 'MESSAGE1'
+    MESSAGE2 = 'MESSAGE2'
 
     def icon(self):
         return QIcon(os.path.join(pluginPath, 'QNEAT3', 'icons', 'icon_servicearea_polygon_missing_import.svg'))
@@ -50,7 +51,7 @@ class DummyAlgorithm(QgisAlgorithm):
         return 'DummyAlgorithmIsoAreas'
 
     def displayName(self):
-        return self.tr('[matplotlib not installed] Iso-Area as Polygon')
+        return self.tr('[matplotlib not installed] Iso-Area as Polygon (open for install help)')
     
     def print_typestring(self, var):
         return "Type:"+str(type(var))+" repr: "+var.__str__()
@@ -59,13 +60,14 @@ class DummyAlgorithm(QgisAlgorithm):
         super().__init__()
 
     def initAlgorithm(self, config=None):
-        self.addParameter(QgsProcessingParameterString(self.MESSAGE, self.tr("Help Message"), self.tr("You need to install matplotLib to enable this algorithm."), True, False),True)
-    
+        self.addParameter(QgsProcessingParameterString(self.MESSAGE1, self.tr("<b>[matplotlib not installed]</b><br>Some QNEAT3 isochrone area algorithms require <b>matplotlib</b> so that they can be executed properly. Depending on your operating system you may install matplotlib <b>manually</b> using the following options:<br><br> <b>Windows</b>: <ol><li>Open the <i>OSGeo4W shell</i> that has been installed alongside QGIS (click <i>Start</i> - type <i>OSGeo4W Shell</i> - hit Enter)</li><li>Copy the command displayed in the textbox below and paste it into the shell</li><li>Accept the installation by typing 'yes' when prompted</li></ol>"), self.tr('python-qgis -m pip install matplotlib'), False, False))
+        self.addParameter(QgsProcessingParameterString(self.MESSAGE2, self.tr("<br><b>Linux</b><ol><li>Open a terminal</li><li>Copy the command displayed below and paste it into the terminal, then hit Enter and confirm installation with 'yes' when prompted</li></ol>"), self.tr("pip install matplotlib"),False,False))
+        
     def processAlgorithm(self, parameters, context, feedback):
-        output_message = self.parameterAsString(parameters, self.MESSAGE, context)
+        output_message = self.parameterAsString(parameters, self.MESSAGE1, context)
         feedback.pushInfo("You need to install matplotlib to enable this algorithm.")
 
         results = {}
-        results[self.MESSAGE] = output_message
+        results[self.MESSAGE1] = 'Refer to the help provided in the algorithm.'
         return results
 
